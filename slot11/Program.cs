441d25e50dac499115a4388cba02ea0dbb2a279d
phamngocchien2004@@ <114908347+phamngocchien2004@users.noreply.github.com>
2023-08-17 16:27:50 +0700

    Update Program

--- slot11/Program.cs
+++ slot11/Program.cs
@@ -1,3 +1,21 @@
-﻿using slot11.amslot11;
+using slot11.amslot11;
 
- class Program
+public class Program
+{
+    static void Main(string[] args)
+    {
+        BankAccount account = new BankAccount();
+
+        account.SoDuThayDoi += Account_SoDuThayDoi;
+
+
+        account.Deposit(1000000);
+        account.Withdraw(1000000);
+    }
+
+    private static void Account_SoDuThayDoi(object sender, EventArgs e)
+    {
+        BankAccount account = (BankAccount)sender;
+        Console.WriteLine($"Bien dong so du: {account.Balance}");
+    }
+}
